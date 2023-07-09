b52f05e5c2af845dab0d7c99b4a42447e7d3c798
DeveloperInside <enesm.hancer@gmail.com>
2023-07-09 17:32:31 +0300

    chore: user seeds created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use App\Models\Vibes;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Enes Hancer',
+            'email' => 'enesm.hancer@gmail.com',
+            'password' => 'enes123.'
+        ]);
+
+        // TODO: create a factory for vibes
+        // Vibes::factory(3)->create();
     }
 }
