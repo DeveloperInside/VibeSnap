a42f0b6801496ad5b179bbf8809278e66f406735
DeveloperInside <enesm.hancer@gmail.com>
2023-07-10 21:19:59 +0300

    feat: user register and login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,3 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('users', function () {
     return response()->json(User::all());
 });
+
+Route::post('login', [UserController::class, 'login']);
+
+Route::post('register', [UserController::class, 'register']);
