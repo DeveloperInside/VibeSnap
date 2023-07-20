a9ec170ad4f4d72da120314a4d58d8bfba2eb86e
DeveloperInside <enesm.hancer@gmail.com>
2023-07-20 18:31:10 +0300

    chore: Database seeder comments deleted

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,22 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()
             ->count(25)
             ->hasVibes(10)
             ->create();
 
-
-
-        // User::factory()->create([
-        //     'name' => 'Enes Hancer',
-        //     'email' => 'enesm.hancer@gmail.com',
-        //     'password' => 'enes123.'
-        // ]);
-
-        // TODO: create a factory for vibes
-        // Vibes::factory(3)->create();
     }
 }
